500f020dfa3df05c5bdf2fc567ac0a7cc7795008
Jorge Jiménez <gexgt90@gmail.com>
2025-12-04 18:23:52 +0100

    add Foodjson

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Bienvenido a la Home Page</h1>
+      <h1>Recipe Book app</h1>
       <p>Esta es la página principal de la aplicación.</p>
     </div>
   );
